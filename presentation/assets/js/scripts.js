61309626997defe8b6f1393d0da471be38641756
Aldee Mativo <07aldee@gmail.com>
2021-01-05 22:41:03 +0800

    Updates

--- presentation/assets/js/scripts.js
+++ presentation/assets/js/scripts.js
@@ -1,8 +1,13 @@
+let timerInterval;
+let blinkInterval;
+let slideInterval;
+
+const photosCount = 3;
+
 function runTimer() {
-  let s = 0;
-  let m = 0;
-  let h = 0;
-  const i = setInterval(() => {
+  let s = 0, m = 0, h = 0;
+
+  timerInterval = setInterval(() => {
 
     s++;
 
@@ -16,26 +21,77 @@ function runTimer() {
       h++;
     }
 
-    document.querySelector('span#hr').innerHTML = (h + '').padStart(2, '0');
-    document.querySelector('span#min').innerHTML = (m + '').padStart(2, '0');
-    document.querySelector('span#sec').innerHTML = (s + '').padStart(2, '0');
+    $('span#hr').html((h + '').padStart(2, '0'));
+    $('span#min').html((m + '').padStart(2, '0'));
+    $('span#sec').html((s + '').padStart(2, '0'));
 
   }, 1000);
 }
 
-function glitch(t) {
+function glitch(t, cb) {
+  $('img#bad-reception').css('visibility', 'visible');
 
-  document.querySelector('img#bad-reception').style.visibility = 'visible';
+  const st = setTimeout(() => {
+    $('img#bad-reception').css('visibility', 'hidden');
+    typeof cb === 'function' && cb();
 
-  setTimeout(() => {
-    console.log('done');
-    document.querySelector('img#bad-reception').style.visibility = 'hidden';
+    clearTimeout(st);
   }, t);
-
 }
 
+function playSlideshow(n) {
+  let i = 0;
+
+  slideInterval = setInterval(() => {
+    const sel = $('#slideshow');
+
+    i++;
+
+    // sel.hide('slide', { direction: 'left' }, () => {
+    //   sel.css('backgroundImage', `url(./assets/compilations/${i}.png)`)
+    //   .show('slide', { direction: 'right' });
+    // });
+    sel.fadeOut(() => {
+      sel
+        .css('backgroundImage', `url(./assets/compilations/${i}.png)`)
+        .fadeIn();
+    });
+
+    if (i >= photosCount) {
+      return clearInterval(slideInterval);
+    }
+  }, n);
+}
+
+function blinkPlayIcon() {
+  blinkInterval = setInterval(() => {
+    const v = $('span#play-blinker').css('visibility');
+    $('span#play-blinker').css('visibility', v === 'visible' ? 'hidden' : 'visible');
+  }, 500);
+}
+
+function reset() {
+  clearInterval(timerInterval);
+  clearInterval(blinkInterval);
+  clearInterval(slideInterval);
+}
+
+function play() {
+  $('div#play-container').remove();
+
+  glitch(3600, () => {
+
+    runTimer();
+    document.querySelector('audio#sound').play();
+    document.querySelector('audio#sound').onended = reset;
+    blinkPlayIcon();
+    playSlideshow(5000);
+
+  });
+}
 
 (function() {
-  glitch(3200);
-  runTimer();
+
+  $('div#play-container').click(play);
+
 })();
